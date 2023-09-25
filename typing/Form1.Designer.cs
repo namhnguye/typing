d562d3fcb35b33c3c15c1f2ac81a9d74402b718e
Nam Nguyen <namnguyen4575@gmail.com>
2023-09-25 02:11:06 -0500

    add base design

--- typing/Form1.Designer.cs
+++ typing/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace typing
 {
-    partial class Form1
+    partial class TYPING
     {
         /// <summary>
         /// Required designer variable.
@@ -28,37 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.sumLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.sumCountLabel = new System.Windows.Forms.Label();
+            this.word = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // titleLabel
             // 
-            this.button1.Location = new System.Drawing.Point(177, 81);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.titleLabel.Font = new System.Drawing.Font("Open Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(228, 43);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "TYPING GAME";
             // 
-            // textBox1
+            // sumLabel
             // 
-            this.textBox1.Location = new System.Drawing.Point(361, 185);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.sumLabel.AutoSize = true;
+            this.sumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumLabel.Location = new System.Drawing.Point(17, 121);
+            this.sumLabel.Name = "sumLabel";
+            this.sumLabel.Size = new System.Drawing.Size(120, 42);
+            this.sumLabel.TabIndex = 3;
+            this.sumLabel.Text = "score:";
             // 
-            // Form1
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(225, 318);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(400, 62);
+            this.inputTextBox.TabIndex = 5;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // sumCountLabel
+            // 
+            this.sumCountLabel.AutoSize = true;
+            this.sumCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumCountLabel.Location = new System.Drawing.Point(134, 123);
+            this.sumCountLabel.Name = "sumCountLabel";
+            this.sumCountLabel.Size = new System.Drawing.Size(39, 42);
+            this.sumCountLabel.TabIndex = 6;
+            this.sumCountLabel.Text = "0";
+            this.sumCountLabel.Click += new System.EventHandler(this.sumCountLabel_Click);
+            // 
+            // word
+            // 
+            this.word.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word.Location = new System.Drawing.Point(225, 157);
+            this.word.Name = "word";
+            this.word.Size = new System.Drawing.Size(400, 63);
+            this.word.TabIndex = 7;
+            this.word.Text = "";
+            // 
+            // TYPING
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.word);
+            this.Controls.Add(this.sumCountLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.sumLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "TYPING";
+            this.Text = "TYPING";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -67,8 +106,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label sumLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label sumCountLabel;
+        private System.Windows.Forms.RichTextBox word;
     }
 }
 
--- typing/Form1.cs
+++ typing/Form1.cs
@@ -10,26 +10,63 @@ using System.Windows.Forms;
 
 namespace typing
 {
-    public partial class Form1 : Form
+    public partial class TYPING : Form
     {
         int sum = 0;
-        public Form1()
+        const string chars = "abcdefghijklmnopqrstuvwxyz";
+
+
+        public TYPING()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            word.Text = generateRandomString();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private string generateRandomString()
         {
-            sum++;
-            textBox1.Text = sum.ToString();
+            var random = new Random();
+            int length = random.Next(4, 8); 
+            char[] stringChars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                stringChars[i] = chars[random.Next(0,26)]; 
+            }
+            string randomString = new string(stringChars);
+            return randomString;
+        }
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == word.Text)
+            {
+                sum++;
+                sumCountLabel.Text = sum.ToString();
+                inputTextBox.Text = "";
+                word.Text = generateRandomString();
+            }
+            else
+            {
+                // Reset the formatting to default for the entire text
+                word.SelectAll();
+                word.SelectionColor = word.ForeColor;
+
+                for (int i = 0; i < word.Text.Length && i < inputTextBox.Text.Length; i++)
+                {
+                    if (inputTextBox.Text[i] != word.Text[i])
+                    {
+                        // Change the color of mismatched letters
+                        word.Select(i, 1);
+                        word.SelectionColor = Color.Red;
+                    }
+                }
+                // Deselect text to avoid further coloring
+                word.DeselectAll();
+            }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void sumCountLabel_Click(object sender, EventArgs e)
         {
 
         }
