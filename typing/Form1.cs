de2636b6cad0c1d01d914a1c4711c74bb96db89e
Nam Nguyen <namnguyen4575@gmail.com>
2023-09-25 00:33:12 -0500

    first commit

--- typing/Form1.cs
+++ typing/Form1.cs
@@ -12,9 +12,26 @@ namespace typing
 {
     public partial class Form1 : Form
     {
+        int sum = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sum++;
+            textBox1.Text = sum.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
